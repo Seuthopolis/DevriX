072e5104d130f1e289dd7d39797c79e9fb19c524
Seuthopolis <m808c@yahoo.com>
2021-09-19 23:06:47 +0300

    Initial Commit

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    const icon = $(".search-icon");
+    const navForm = $("form.search");
+    const weatherLink = $(".weather-link");
+    const opn = $(".opn");
+
+    icon.click(function() {
+        if($(this).hasClass("opn")) {
+            console.log($(this));
+                // $(this).removeClass("ml-3 opn");
+            $(this).hide(200).fadeIn().removeClass("ml-3 opn");
+            navForm.removeClass("spread-out");
+            setTimeout(function() {
+                icon.parent("div").addClass("separator");
+                weatherLink.removeClass("d-none");
+            }, 300);
+        } else {
+            $(this).parent("div").removeClass("separator");
+            $(this).hide().delay(80).fadeIn().addClass("ml-3 opn");
+            weatherLink.addClass("d-none");
+            navForm.addClass("spread-out");
+            if (navForm.hasClass("spread-out")) {
+                navForm.children("input.search").focus();
+            }
+        }
+    });
+});
+
